85cd01cf4f9af52fb8f0d4cbb9fd4ab431709a16
JuanV3rgas <juan.variza@gmail.com>
2025-09-28 22:18:00 -0500

    Implementacion de todos los componentes de header, landing, footer y home en Angular 16.

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AppComponent } from './app.component';
 import { ServiceTableComponent } from './service/service-table/service-table.component';
 import { ServiceDetailComponent } from './service/service-detail/service-detail.component';
 import { ServiceFormComponent } from './service/service-form/service-form.component';
+import { HomePageComponent } from './home/home-page/home-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LandingComponent } from './components/landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServiceTableComponent,
     ServiceDetailComponent,
-    ServiceFormComponent
+    ServiceFormComponent,
+    HomePageComponent,
+    FooterComponent,
+    HeaderComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
