174fd463857dd499aea6b2eb6a0f6547e5e2ee1a
salmanzag <s.almanzag@javeriana.edu.co>
2025-09-30 14:53:57 -0500

    enrutamiento funcinando

--- proyecto/src/app/app-routing.module.ts
+++ proyecto/src/app/app-routing.module.ts
@@ -2,22 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home/home-page/home-page.component';  
 import { ServiceTableComponent } from './service/service-table/service-table.component';
-import {HabitacionTableComponent} from './habitacion/habitacion-table/habitacion-table.component'
-import {RoomTableComponent} from './room/room-table/room-table.component'
+import { HabitacionTableComponent } from './habitacion/habitacion-table/habitacion-table.component'
+import { RoomTableComponent } from './room/room-table/room-table.component'
 
 const routes: Routes = [
   //definimos rutas
-  { path: '', component: HomePageComponent },//Importamos el comonente del main
-  { path: 'Service-Table', component: ServiceTableComponent },//Importamos el comonente del detalle del servicio
-  { path: 'Habitacion-Table', component: HabitacionTableComponent },//Importamos el comonente del detalle de la habitacion
-  { path: 'Room-Table', component: RoomTableComponent },//Importamos el comonente del detalle de la habitacion
-  { path: '**', redirectTo: '' }
+  { path: 'servicio/table', component: ServiceTableComponent },//Importamos el comonente del detalle del servicio
+  { path: 'habitacion/table', component: HabitacionTableComponent },//Importamos el comonente del detalle de la habitacion
+  { path: 'room/table', component: RoomTableComponent },//Importamos el comonente del detalle de la habitacion
+  { path: '**', redirectTo: '' },
+    { path: '', component: HomePageComponent },//Importamos el comonente del main
+
 ];
 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-  
+
 })
 export class AppRoutingModule { }
