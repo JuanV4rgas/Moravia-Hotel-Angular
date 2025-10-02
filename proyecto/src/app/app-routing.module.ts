fd7ac278f92d78515451ee23c109bf37990ebcfe
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-10-02 15:01:11 -0500

    Navegacion completa

--- proyecto/src/app/app-routing.module.ts
+++ proyecto/src/app/app-routing.module.ts
@@ -4,15 +4,18 @@ import { HomePageComponent } from './home/home-page/home-page.component';
 import { ServiceTableComponent } from './service/service-table/service-table.component';
 import { HabitacionTableComponent } from './habitacion/habitacion-table/habitacion-table.component'
 import { RoomTableComponent } from './room/room-table/room-table.component'
+import { ClienteTableComponent } from './cliente/cliente-table/cliente-table.component'
+
 
 const routes: Routes = [
   //definimos rutas
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
   { path: 'servicio/table', component: ServiceTableComponent },//Importamos el comonente del detalle del servicio
   { path: 'habitacion/table', component: HabitacionTableComponent },//Importamos el comonente del detalle de la habitacion
   { path: 'room/table', component: RoomTableComponent },//Importamos el comonente del detalle de la habitacion
-  { path: '**', redirectTo: '' },
-    { path: '', component: HomePageComponent },//Importamos el comonente del main
-
+  { path: 'cliente/table', component: ClienteTableComponent },
+  { path: '**', redirectTo: 'home' },
 ];
 
 
