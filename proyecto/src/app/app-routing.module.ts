9289d950cc6bc321e2b9983ce20748f044e861b6
salmanzag <s.almanzag@javeriana.edu.co>
2025-10-08 15:53:24 -0500

    CRUD rooms listo

--- proyecto/src/app/app-routing.module.ts
+++ proyecto/src/app/app-routing.module.ts
@@ -4,7 +4,10 @@ import { HomePageComponent } from './home/home-page/home-page.component';
 import { ServiceTableComponent } from './servicio/service-table/service-table.component';
 import { ServiceDetailComponent } from './servicio/servicio-detail/service-detail.component';
 import { ServiceFormComponent } from './servicio/service-form/service-form.component';
+import { ServiceListComponent } from './servicio/service-list/service-list.component';
 import { RoomTableComponent } from './room/room-table/room-table.component';
+import { RoomFormComponent } from './room/room-form/room-form.component';
+import { RoomDetailComponent } from './room/room-detail/room-detail.component';
 import { ClienteTableComponent } from './cliente/cliente-table/cliente-table.component';
 import { AuthWrapComponent } from './auth/auth-wrap/auth-wrap.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
@@ -12,7 +15,6 @@ import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component
 import { RoomTypeTableComponent } from './roomtype/roomtype-table/roomtype-table.component';
 import { RoomTypeFormComponent } from './roomtype/roomtype-form/roomtype-form.component';
 import { RoomTypeDetailComponent } from './roomtype/room_type-detail/roomtype-detail.component';
-import { ServiceListComponent } from './servicio/service-list/service-list.component';
 import { RoomtypeListComponent } from './roomtype/roomtype-list/roomtype-list.component';
 
 const routes: Routes = [
@@ -25,13 +27,15 @@ const routes: Routes = [
       { path: 'servicio/table', component: ServiceTableComponent },
       { path: 'servicio/detail', component: ServiceDetailComponent },
       { path: 'servicio/nueva', component: ServiceFormComponent },
+      { path: 'servicio/lista', component: ServiceListComponent },
       { path: 'room/table', component: RoomTableComponent },
+      { path: 'room/nueva', component: RoomFormComponent },
+      { path: 'room/detail', component: RoomDetailComponent },
       { path: 'cliente/table', component: ClienteTableComponent },
       { path: 'roomtype/table', component: RoomTypeTableComponent },
       { path: 'roomtype/lista', component: RoomtypeListComponent },
       { path: 'roomtype/nueva', component: RoomTypeFormComponent },
       { path: 'roomtype/:id', component: RoomTypeDetailComponent },
-      { path: 'servicio/lista', component: ServiceListComponent },
     ],
   },
   {
