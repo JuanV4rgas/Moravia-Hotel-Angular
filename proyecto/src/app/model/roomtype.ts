f02cb1f5038ea986d841a9f68420740758c55396
David Rodriguez <rodriguezj.david@javeriana.edu.co>
2025-10-30 12:18:28 -0500

    Create roomtype.ts

---
+++ proyecto/src/app/model/roomtype.ts
@@ -0,0 +1,10 @@
+export interface RoomType {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  capacity: string;
+  numberOfBeds: number;
+  image: string;
+  type: string; 
+}
